b0dd2f87f022e811c97175561460af6defef1129
HushBugger <hushbugger@posteo.net>
2025-06-08 00:09:16 +0200

    dr: Add modernized dump of chapters 1-4

---
+++ deltarune/text/extract_textdump.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python3
+"""Generate a lang.json for all chapters in both English and Japanese."""
+
+import io
+import json
+import pathlib
+import re
+import subprocess
+import sys
+
+# Expected directory structure:
+# ├── 1
+# │   ├── CodeEntries
+# │   └── lang
+# │       ├── lang_en.json
+# │       └── lang_ja.json
+# ├── 2
+# │   ├── CodeEntries
+# │   └── lang
+# │       (etc...)
+# ├── 3
+# └── 4
+# Originally extracted with UnderTale Mod Tool v0.8.1.1
+try:
+    source = pathlib.Path(sys.argv[1])
+except IndexError:
+    print(f"Usage: {sys.argv[0]} path/to/deltarune", file=sys.stderr)
+    sys.exit(1)
+
+
+type FunArgs = list[str | None]
+type FunCall = tuple[str, FunArgs]
+
+
+def parse_args(text: str) -> FunArgs:
+    args = []
+    i = 0
+    while i < len(text):
+        if text[i] in (",", " "):
+            i += 1
+        elif text[i] == '"':
+            i += 1
+            arg = io.StringIO()
+            while text[i] != '"':
+                if text[i] == "\\":
+                    i += 1
+                    if text[i] == "\\":
+                        arg.write("\\")
+                    elif text[i] == '"':
+                        arg.write('"')
+                    elif text[i] == "n":
+                        arg.write("\n")
+                    elif text[i] == "t":
+                        arg.write("\t")
+                    elif text[i] == "f":
+                        # 90% sure this is just a missing backslash
+                        # but let's stay faithful
+                        arg.write("\f")
+                    else:
+                        assert False, text
+                    i += 1
+                else:
+                    arg.write(text[i])
+                    i += 1
+            i += 1
+            args.append(arg.getvalue())
+        elif text[i] == ")":
+            break
+        else:
+            args.append(None)
+            depth = 0
+            while i < len(text):
+                if text[i] == "(":
+                    depth += 1
+                elif text[i] == ")":
+                    depth -= 1
+                    if depth < 0:
+                        break
+                elif text[i] == "," and depth == 0:
+                    break
+                i += 1
+    return args
+
+
+TEXTFUNCS = [
+    "stringsetloc",
+    "msgsetloc",
+    "msgnextloc",
+    "stringsetsubloc",
+    "msgsetsubloc",
+    "msgnextsubloc",
+    "scr_84_get_lang_string",
+]
+RE_TEXTFUNCS = re.compile(f"({'|'.join(TEXTFUNCS)})\\(")
+
+
+def parse_line(line: str) -> list[FunCall]:
+    if line.startswith("function "):
+        return []
+    calls: list[FunCall] = []
+    for match in RE_TEXTFUNCS.finditer(line):
+        func = match.group(1)
+        args = parse_args(line[match.end() :])
+        calls.append((func, args))
+    assert calls, line
+    return calls
+
+
+CHAPTERS = [1, 2, 3, 4]
+text = {n: {} for n in CHAPTERS}
+
+for n in CHAPTERS:
+    path = source / str(n)
+    ja: dict[str, str] = json.loads((path / "lang" / "lang_ja.json").read_text())
+    text[n]["ja"] = ja
+    if n == 1:
+        text[n]["en"] = json.loads((path / "lang" / "lang_en.json").read_text())
+        continue
+    en: dict[str, str] = {}
+    text[n]["en"] = en
+
+    rg = subprocess.run(
+        [
+            "rg",
+            "--sort=path",
+            "--no-filename",
+            f"({'|'.join(TEXTFUNCS)})\\([^)]",
+            path / "CodeEntries",
+        ],
+        stdout=subprocess.PIPE,
+        check=True,
+    ).stdout.decode()
+
+    for line in rg.splitlines():
+        for func, args in parse_line(line):
+            match func, args:
+                case "scr_84_get_lang_string", [None]:
+                    pass
+                case "scr_84_get_lang_string", [str(arg)]:
+                    en[arg] = text[1]["ja"][arg]
+                case "msgsetloc", [None, str(trans)]:
+                    pass
+                case "msgsetsubloc", [None, str(), None]:
+                    pass
+                case (
+                    ("stringsetloc", [str(trans), str(key)])
+                    | ("msgsetsubloc", [_, str(trans), *_, str(key)])
+                    | ("msgnextsubloc", [str(trans), *_, str(key)])
+                    | ("stringsetsubloc", [str(trans), *_, str(key)])
+                    | ("msgsetloc", [_, str(trans), str(key)])
+                    | ("msgnextloc", [str(trans), str(key)])
+                ):
+                    en[key] = trans
+                case _:
+                    print(func, args, line, file=sys.stderr)
+                    sys.exit(1)
+
+with open("lang.json", "w") as f:
+    json.dump(text, f, indent=0, ensure_ascii=False, sort_keys=True)
---
+++ deltarune/text/render_textdump.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+"""Convert lang.json to the data we want to show on the page."""
+
+import io
+import json
+import sys
+import typing
+
+
+def render(text: str | None) -> str | None:
+    if not text:
+        return None
+    out = io.StringIO()
+    color = "W"
+    i = 0
+    while i < len(text):
+        match text[i]:
+            case "\\":
+                match text[i + 1]:
+                    case "c":
+                        prev_color = color
+                        if text[i + 1] == "c":
+                            color = text[i + 2]
+                        if color != prev_color:
+                            if prev_color != "W":
+                                out.write("</span>")
+                            if color != "W":
+                                out.write(f'<span class="{color}">')
+                    case "O" | "I":
+                        # TODO: Tenna text, interface buttons
+                        out.write('<span class="picture">[IMG]</span>')
+                        while text[i + 3] in (" ", "\u3000"):
+                            i += 1
+                    case "M" | "E" | "T" | "F" | "S" | "s":
+                        # Modifiers at the start of a message
+                        # M = ?
+                        # E = emotion?
+                        # T = typer?
+                        # F = face?
+                        # S = sound
+                        # s = different sounds?
+                        pass
+                    case "a":
+                        # Something to do with Japanese text?
+                        pass
+                    case "f":
+                        # Something at the end of a message?
+                        pass
+                    case "C":
+                        # Multiple choice
+                        pass
+                    case "U":
+                        # This shows up in just one message, no clue
+                        pass
+                    case "m":
+                        # Sweet/Cap'n/KK faces?
+                        pass
+                    case ch:
+                        print(ch)
+                        print(text)
+                        sys.exit(1)
+                i += 2
+            case "/":
+                if i + 1 < len(text) and text[i + 1] == "*":
+                    i += 1
+            case "&" | "#":
+                out.write("\n")
+            case "\t":
+                out.write(" ")  # TODO
+            case "^":
+                if text[i + 1].isdigit():
+                    i += 1
+            case "%":
+                pass
+            case ">":
+                out.write("&gt;")
+            case "<":
+                out.write("&lt;")
+            case "`":
+                out.write(text[i + 1])
+                i += 1
+            case char:
+                out.write(char)
+        i += 1
+    if color != "W":
+        out.write("</span>")
+    return out.getvalue()
+
+
+lang: dict[str, dict[typing.Literal["en", "ja"], dict[str, str]]] = json.load(
+    open("lang.json")
+)
+rendered = {}
+for n in lang:
+    rendered[n] = {}
+    ks = sorted(lang[n]["en"].keys() | lang[n]["ja"].keys())
+    for k in ks:
+        en = lang[n]["en"].get(k)
+        ja = lang[n]["ja"].get(k)
+        if (en and en.strip(" \\C234")) or (ja and ja.strip(" \\C234")):
+            ren = render(en)
+            rja = render(ja)
+            if (ren and ren.strip()) or (rja and rja.strip()):
+                rendered[n][k] = {"en": ren, "ja": rja}
+
+json.dump(
+    rendered, open("rendered.json", "w"), indent=0, ensure_ascii=False, sort_keys=True
+)
