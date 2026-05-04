4dddcab1a3a0aec3bc22be187631f3d7c582e75e
HushBugger <hushbugger@posteo.net>
2026-05-04 22:48:25 +0200

    dr: beta: Use portable regex match loop

--- deltarune/text/render_textdump.py
+++ deltarune/text/render_textdump.py
@@ -677,6 +677,7 @@ def dumpbin():
         def get(self) -> str:
             value = self.buf.getvalue()
             # Ensure that UTF-16 offsets (JS) match UTF-32 offsets (Python)
+            # If this fails then the RegExp.exec() loop also needs tweaking
             assert max(map(ord, value)) <= 65535
             return value
 
