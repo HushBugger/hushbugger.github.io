5cc66517bb887e282a6be86dbd29541d77e2498e
HushBugger <hushbugger@posteo.net>
2026-05-05 21:13:37 +0200

    dr: beta: Add search modifiers

--- deltarune/text/render_textdump.py
+++ deltarune/text/render_textdump.py
@@ -677,7 +677,7 @@ def dumpbin():
         def get(self) -> str:
             value = self.buf.getvalue()
             # Ensure that UTF-16 offsets (JS) match UTF-32 offsets (Python)
-            # If this fails then the RegExp.exec() loop also needs tweaking
+            # If this fails then the highlightElem() and compileQuery() need tweaking
             assert max(map(ord, value)) <= 65535
             return value
 
